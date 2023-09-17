b2a5a86356a4100c4ba018efb06ce7fe1104c606
JB Ladera <domobaby10@gmail.com>
2023-09-16 20:06:08 -0500

    finish next and nextline functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Cell from './structs/Cell';
+import { delay } from './util/delay';
+
+const delayTime = 200;
+const maxVisible = 10;
 
 function App() {
-  const [input, setInput] = useState('');
-  const [inputPointer, setInputPointer] = useState(0);
-  const [displayPointer, setDisplayPointer] = useState(0);
+  const inputPointer = useRef(0);
+  const displayPointer = useRef(0);
+  const isExecutingFunction = useRef(false);
+
+  const [input, setInput] = useState([]);
   const [cells, setCells] = useState(createCells());
-
-
+  const [resultText, setResultText] = useState('');
 
   useEffect(() => {
-    setInputPointer(0);
-    setDisplayPointer(0);
     setCells(createCells());
+    setResultText('');
   }, [input]);
 
+  function copyArray(arr) {
+    return arr.map(e => Object.assign({}, e));
+  }
+
+  function createInput(str) {
+    const ret = [];
+    for (let i = 0; i < str.length; i++) {
+      ret.push({ value: str[i], read: false, skipped: false });
+    }
+    return ret;
+  }
+
   function createCells() {
-    const maxVisible = 10;
     const ret = [];
 
-    for (let i = displayPointer; i < displayPointer + maxVisible; i++) {
+    for (let i = displayPointer.current; i < displayPointer.current + maxVisible; i++) {
       if (i < input.length) {
-        ret.push(<Cell key={i} value={input[i]} />)
+        ret.push(<Cell key={i} {...input[i]} />)
       } else {
         ret.push(<Cell key={i} empty={true} />)
       }
@@ -31,26 +46,107 @@ function App() {
     return ret;
   }
 
+  function translateResult(result) {
+    if (typeof result === 'undefined') {
+      return 'NoSuchElementException';
+    }
+
+    if (result === '') {
+      return '(empty string)';
+    }
+
+    return result;
+  }
+
   /**
    * @param {React.MouseEvent<HTMLButtonElement,MouseEvent>} e 
    */
   function clickEnter(e) {
     const textarea = e.currentTarget.ownerDocument.querySelector('textarea');
-    setInput(textarea.value);
+    inputPointer.current = 0;
+    displayPointer.current = 0;
+    setInput(createInput(textarea.value));
   }
 
-  /**
-   * @param {React.MouseEvent<HTMLButtonElement,MouseEvent>} e 
-   */
-  function clickNext(e) {
+  async function readToken(data, pointer, consumeNewLine, predicate) {
+    let ret = '';
 
+    while (pointer < data.length && predicate(data[pointer].value)) {
+      if (pointer === displayPointer.current + maxVisible) {
+        displayPointer.current += maxVisible;
+        setInput(copyArray(data));
+        await delay(delayTime);
+      }
+
+      ret += data[pointer].value;
+
+      data[pointer].read = true;
+      setInput(copyArray(data));
+      await delay(delayTime);
+
+      pointer++;
+    }
+
+    if (consumeNewLine && pointer < data.length) {
+      if (pointer === displayPointer.current + maxVisible) {
+        displayPointer.current += maxVisible;
+        setInput(copyArray(data));
+        await delay(delayTime);
+      }
+
+      data[pointer].skipped = true;
+      setInput(copyArray(data));
+      await delay(delayTime);
+
+      pointer++;
+    }
+
+    inputPointer.current = pointer;
+    return ret === '' ? undefined : ret;
   }
 
-  /**
-   * @param {React.MouseEvent<HTMLButtonElement,MouseEvent>} e 
-   */
-  function clickNextLine(e) {
+  async function next(data, pointer) {
+    function isWhitespace(e) {
+      return e === ' ' || e === '\n';
+    }
 
+    while (pointer < data.length && isWhitespace(data[pointer].value)) {
+      if (pointer === displayPointer.current + maxVisible) {
+        displayPointer.current += maxVisible;
+        setInput(copyArray(data));
+        await delay(delayTime);
+      }
+
+      data[pointer].skipped = true;
+      setInput(copyArray(data));
+      await delay(delayTime);
+
+      pointer++;
+    }
+
+    return await readToken(data, pointer, false, (e) => !isWhitespace(e));
+  }
+
+  async function executeNext() {
+    if (!isExecutingFunction.current) {
+      isExecutingFunction.current = true;
+      const res = await next(copyArray(input), inputPointer.current);
+      setResultText(translateResult(res));
+      isExecutingFunction.current = false;
+    }
+  }
+
+  async function nextLine(data, pointer) {
+    return await readToken(data, pointer, true, (e) => e !== '\n');
+  }
+
+  async function executeNextLine() {
+    if (!isExecutingFunction.current) {
+      isExecutingFunction.current = true;
+      const res = await nextLine(copyArray(input), inputPointer.current);
+      setResultText(translateResult(res));
+      isExecutingFunction.current = false;
+    }
   }
 
   return (
@@ -61,12 +157,12 @@ function App() {
       <div className="row" style={{ width: 900 }}>
         {cells}
       </div>
-      <button className="next" onClick={clickNext}>Next</button>
-      <button className="nextline" onClick={clickNextLine}>NextLine</button>
+      <button className="next" onClick={executeNext}>Next</button>
+      <button className="nextline" onClick={executeNextLine}>NextLine</button>
       <br />
       <div className="results">
         <span className="result-title">Result: </span>
-        <span className="result">{ }</span>
+        <span className="result">{resultText}</span>
       </div>
       <br />
       <ul className="color-key">
--- src/constants/index.js
+++
@@ -1,3 +0,0 @@
-export const EMPTY_STRING = '(empty string)';
-export const NO_SUCH_ELEMENT_EXCEPTION = 'NoSuchElementException';
-export const NO_INPUT = 'Enter input first';
